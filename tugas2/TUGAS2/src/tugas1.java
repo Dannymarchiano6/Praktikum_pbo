fe716f8c096af787219aba11463a8a1ad289ffb7
Azunyan25 <98401834+Dannymarchiano6@users.noreply.github.com>
2022-12-17 14:09:20 +0700

    Add files via upload
    Tugas 2 Praktikum

---
+++ tugas2/TUGAS2/src/tugas1.java
@@ -0,0 +1,13 @@
+public class tugas1 {
+    public static void main(String [] args){
+
+        int a = 10;
+        int b = 10;
+
+        a++;
+        System.out.println("Isi variabel a :" + a);
+
+        ++b;
+        System.out.println("isi variabel b :" + b);
+    }
+}
---
+++ tugas2/TUGAS2/src/tugas5.java
@@ -0,0 +1,10 @@
+public class tugas5 {
+    public static void main(String[] args) {
+        double alas = 10;
+        double tinggi = 5;
+
+        //menghitung luas segitiga
+        double luas = (alas * tinggi) / 2;
+        System.out.println("Luas segitiga dengan alas"+ alas + " dan tinggi " + tinggi + "adalah" + luas);
+    }
+}
