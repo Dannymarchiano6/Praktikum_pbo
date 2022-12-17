b954463242f61c1d9e4a4d11ab3948629ab68bd2
Azunyan25 <98401834+Dannymarchiano6@users.noreply.github.com>
2022-12-17 12:17:24 +0700

    Tugas Praktikum 1
    TUGAS PRAKTIKUM PBO UNIVERSITAS SURYAKANCANA

---
+++ Praktikum1/Tugas1/src/sukabelajar.java
@@ -0,0 +1,7 @@
+public class sukabelajar {
+    public static void main(String[] args) {
+        System.out.println("Saya suka Java");
+        System.out.println("Belajar Java Asik");
+        System.out.println("Yuk kita enjoy");
+    }
+}
---
+++ Praktikum1/Tugas1/src/unsur.java
@@ -0,0 +1,8 @@
+public class unsur {
+    public static void main(String[] args) {
+        System.out.println("Universitas SuryaKancana");
+        System.out.println("Universitas pilihan di Cianjur");
+        System.out.println("Kuliah di Fakultas teknik");
+        System.out.println("Jurusan Teknik Informatika");
+    }
+}
